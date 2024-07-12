7d71098da5b4680c034c59d4d1e9e82ee8fb5e46
Znpos901 <115646234+Znpos901@users.noreply.github.com>
2024-07-12 18:51:49 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,7 @@
 @echo off
 pushd %~dp0
-set NODE_ENV=dev
 call npm install --no-audit --no-fund --quiet
-call npm install --save-dev @swc/core @swc/cli @swc/register
+call npm install --save-dev @swc/core @swc/cli @swc-node/register
 node start.js
 pause
 popd
